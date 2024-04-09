9c467a735f6e8649b897fed30400d6e2c5a5cdf5
ManonB15 <bourrey.m@gmail.com>
2024-04-09 11:55:28 +0200

    Ajout des lignes sur la rentablilité

--- produit_object.js
+++ produit_object.js
@@ -15,3 +15,12 @@ produit.valeur_stock = produit.price*produit.stock
 
 console.log("Produit :", produit.name, "/ Gain", produit.gain, "/ Valeur stock", produit.valeur_stock)
 
+produit.cost = 20
+produit.total_cost = produit.cost * (produit.number_sell+produit.stock)
+console.log("Somme d'achâts des produits :", produit.total_cost)
+
+let benefice_par_produit = produit.price - produit.cost
+console.log("Bénéfice par produit", benefice_par_produit)
+
+let pourcentage_benefice = 100 * (benefice_par_produit / produit.price)
+console.log("Bénéfice par produit :", pourcentage_benefice + "% ~", Math.round(pourcentage_benefice) + "%")
