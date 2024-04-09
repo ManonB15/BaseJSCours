48d862c75156ee766be0394cd5a2bd1fb9d2c002
ManonB15 <bourrey.m@gmail.com>
2024-04-09 09:20:17 +0200

    Ajout de l'exercice Nombre livre vendus

---
+++ moyenne_par_j_livre_vendu.js
@@ -0,0 +1,4 @@
+let LivreVendusSemaine = 650                                      // Déclaration de la variable du nb de livre vendus par semaine
+LivreVendusSemaine += 50                                          // Ajout de 50 livres à la variable
+let LivreVenduJour = LivreVendusSemaine / 7                       // Déclaration de la variable pour connaître le nb de livre vendu par jour
+console.log("Le nombre de livre vendu est de" , LivreVenduJour , "livres")      // Affichage de la variable de livre vendu par jour
